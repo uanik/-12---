f1a756816b3295a5f64ba893e197d10d280153d3
uanik <uanik.com.ua@gmail.com>
2020-06-27 14:36:18 +0300

    добавление проекта А12Е

--- datchik/Form1.Designer.cs
+++ datchik/Form1.Designer.cs
@@ -29,12 +29,54 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.Timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Timer1
+            // 
+            this.Timer1.Enabled = true;
+            this.Timer1.Interval = 200;
+            this.Timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(429, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer Timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- datchik/Form1.cs
+++ datchik/Form1.cs
@@ -1,20 +1,78 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.IO.Ports;
 using System.Windows.Forms;
 
 namespace datchik
 {
     public partial class Form1 : Form
     {
+        FileInfo aFile = new FileInfo("WEIGHT.TXT");
+        SerialPort SerialPort1 = new SerialPort("COM4");
+        SerialPort SerialPort2 = new SerialPort("COM3");
         public Form1()
         {
             InitializeComponent();
+            DateTime sysDate = DateTime.Today;
+            SerialPort1.BaudRate = 19200; //для vkmodule       
+            SerialPort1.Parity = Parity.None;
+            SerialPort1.StopBits = StopBits.One;
+            SerialPort1.DataBits = 8;
+            SerialPort1.Open();
+            SerialPort2.BaudRate = 9600; //для A12E  
+            SerialPort2.Parity = Parity.None;
+            SerialPort2.StopBits = StopBits.One;
+            SerialPort2.DataBits = 8;
+            SerialPort2.Open();
+
+            if (WindowState == FormWindowState.Minimized) // для трея
+            { Hide(); }
+            if (aFile.Exists == false) { aFile.Create(); }
+            aFile.Attributes = FileAttributes.Normal;
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            byte[] vals = { 0x00, 0x02, 0x00, 0x00, 0x00, 0x08, 0x78, 0x1D };   //для vkmodule
+            if (SerialPort1.IsOpen) SerialPort1.Write(vals, 0, 8);
+          //  SerialPort1.DiscardInBuffer();
+            System.Threading.Thread.Sleep(50);
+            int byteRecieved = SerialPort1.BytesToRead;
+            byte[] messByte = new byte[byteRecieved];
+            SerialPort1.Read(messByte, 0, byteRecieved);
+            int input;
+            input = 64;
+            if (byteRecieved == 6)
+                input = messByte[3];
+            string stroka = input.ToString();
+            
+
+            label1.Text = stroka;
+            //------------- чтение данных из весопроцессора
+            if (SerialPort2.IsOpen) { }
+            SerialPort2.DiscardInBuffer();
+            System.Threading.Thread.Sleep(150);
+            string input1 = SerialPort2.ReadExisting();
+         //   label2.Text = input1;
+            int idx;
+                              //-------- парсер
+                idx = input1.IndexOf("WN");
+
+                if (idx > -1)
+                {
+                    input1 = input1.Substring(idx + 2, 7);
+                                    }
+                idx = input1.IndexOf("kg");
+                if (idx == 5)
+                {
+                    input1 = input1.Substring(0, 5);
+                    string ves = input1;
+                }
+            stroka = stroka + "   " + input1;
+            label2.Text = input1;
+            System.IO.File.WriteAllText(@"WEIGHT.TXT", stroka);
+        }             
+      
+        }
+
     }
-}
